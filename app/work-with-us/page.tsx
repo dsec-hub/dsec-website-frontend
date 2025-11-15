8192c30320137ca52c625c681763b80dab9b6ecc
Clupai8o0 <clupaio4@gmail.com>
2025-11-15 12:46:17 +1100

    adding the package next transitions

---
+++ app/work-with-us/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WorkWithUsPage() {
+  return (
+    <div>WorkWithUsPage</div>
+  )
+}
+
+export default WorkWithUsPage
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client";
+
+import ColorBends from './ColorBends';
 
 const Hero = () => {
   return (
-    <section>Hero</section>
-  )
+		<section className="mx-4 my-4 bg-card rounded-4xl overflow-hidden h-[70vh]">
+			<ColorBends
+				// colors={["#ff5c7a", "#8a5cff", "#00ffd1"]}
+				rotation={160}
+				speed={0.3}
+				scale={1.4}
+				frequency={1}
+				warpStrength={1}
+				mouseInfluence={0.8}
+				parallax={0.6}
+				noise={0.08}
+				transparent
+			/>
+		</section>
+	);
 }
 
 export default Hero
